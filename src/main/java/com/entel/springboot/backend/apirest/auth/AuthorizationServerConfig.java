d73494f38c3d003283bcfc5c1c8b5915d35a091d
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-02 17:47:02 -0500

    [INFO] - Autenticación: Añadiendo configuración de las aplicaciones clientes y acceso a endpoints.

--- src/main/java/com/entel/springboot/backend/apirest/auth/AuthorizationServerConfig.java
+++ src/main/java/com/entel/springboot/backend/apirest/auth/AuthorizationServerConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -27,23 +25,38 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Qualifier("authenticationManager")
 	private AuthenticationManager authenticationManager;
 
+	// Los dos endpoint estan protegidos por autentificación via http basic (Header Authorization Basic: Client id + Client secret)
 	@Override
-	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception { // Configuración de permisos de rutas de acceso
 		
-		super.configure(security);
+		security.tokenKeyAccess("permitAll()") // Permiso para iniciar sesion a todos en el endpoint oauth/token
+		.checkTokenAccess("isAuthenticated()"); // Permiso al endpoint que se encarga de validar el token oauth/check_token
 	}
 
 	@Override
-	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception { // Configuración de aplicaciones que van a acceder al apirest
 		
-		super.configure(clients);
+		clients.inMemory().withClient("angularapp").secret(passwordEncoder.encode("123456"))
+		.scopes("read", "write")
+		.authorizedGrantTypes("password","refresh_token") // code: codigo de autorización | implicita: aplicaciones públicas | refresh_token : Se obtiene nuevo token antes que caduque
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600);
+		/*
+		.and()
+		.withClient("androidapp")
+		.secret(passwordEncoder.encode("67890"))
+		.scopes("read", "write")
+		.authorizedGrantTypes("password", "refresh_token")
+		.accessTokenValiditySeconds(20000)
+		.refreshTokenValiditySeconds(20000);
+		*/
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		
 		endpoints.authenticationManager(authenticationManager)
-		.tokenStore(tokenStore())
+		.tokenStore(tokenStore()) // Opcional => Explicito, por debajo ya lo implementa 
 		.accessTokenConverter(accessTokenConverter()); // Encargado de manejar datos del token (autenticación del usuario, roles, etc)
 		
 	}
