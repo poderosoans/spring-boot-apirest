651a5b2985d3e6c4d24afbae8ae366e9be4cb943
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-02 15:54:52 -0500

    [INFO] - Creación de las entidades User y Role

---
+++ src/main/java/com/entel/springboot/backend/apirest/models/entity/Role.java
@@ -0,0 +1,51 @@
+package com.entel.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true, length = 20)
+	private String name;
+	
+	/*
+	// Usuario: es dueño de la relación para que sea bidireccional | el rol es la relación inversa.
+	@ManyToMany(mappedBy="roles")
+	private List<User> users;
+	*/
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
