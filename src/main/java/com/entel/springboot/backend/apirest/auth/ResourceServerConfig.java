89ef66a99e169608947bf9bd57fca9cfd6139f95
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-03 18:51:08 -0500

    [INFO] - Autenticación: Configurando Cors en Spring Security.

--- src/main/java/com/entel/springboot/backend/apirest/auth/ResourceServerConfig.java
+++ src/main/java/com/entel/springboot/backend/apirest/auth/ResourceServerConfig.java
@@ -1,10 +1,19 @@
 package com.entel.springboot.backend.apirest.auth;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -13,12 +22,35 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clients","/api/clients/page/**","/api/uploads/img/**").permitAll()
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clients","/api/clients/page/**","/api/uploads/img/**","/images/**").permitAll()
 		/*.antMatchers(HttpMethod.GET, "/api/clients/{id}").hasAnyRole("USER", "ADMIN")
 		.antMatchers(HttpMethod.POST, "/api/clients/upload").hasAnyRole("USER", "ADMIN")
 		.antMatchers(HttpMethod.POST, "/api/clients").hasRole("ADMIN")
 		.antMatchers("/api/clients/**").hasRole("ADMIN") */
-		.anyRequest().authenticated();
+		.anyRequest().authenticated()
+		.and().cors().configurationSource(corsConfigurationSource());
+	}
+	
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT", "DELETE", "OPTIONS")); // OPTIONS: Cuando se envía una solicitud para obtener el token en algunos navegadores 
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+	
+	// Configurando un filtro en la prioridad mas alta de los filtros de Spring, se aplica a los servidores de autorizacion y cada vez que se valida el token una vez que estemos autenticados
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter (corsConfigurationSource()));
+		// Dar un orden bajo, ya que mayor es la prioridad
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 	
 	
