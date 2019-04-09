2f883a6ccc286587561124734af38c812ae832f5
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-08 19:49:36 -0500

    [INFO] - Sistema Facturación: Creando el controlador FacturaRestController con la acción handler show.

---
+++ src/main/java/com/entel/springboot/backend/apirest/controllers/InvoiceRestController.java
@@ -0,0 +1,28 @@
+package com.entel.springboot.backend.apirest.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.entel.springboot.backend.apirest.models.entity.Invoice;
+import com.entel.springboot.backend.apirest.models.services.IClientService;
+
+@CrossOrigin(origins= {"http:localhost:4200"})
+@RestController
+@RequestMapping("/api")
+public class InvoiceRestController {
+	@Autowired
+	private IClientService clientService;
+	
+	@GetMapping("/invoices/{id}")
+	@ResponseStatus(code=HttpStatus.OK)
+	public Invoice show(@PathVariable Long id) {
+		
+		return clientService.findInvoiceById(id);
+	}
+}
