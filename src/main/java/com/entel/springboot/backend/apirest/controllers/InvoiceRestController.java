81fd2e0d2b35438871cdc909534d875209aa749d
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-09 18:53:55 -0500

    [INFO] - Sistema Facturación: Añadiendo el método para crear facura en el controller.

--- src/main/java/com/entel/springboot/backend/apirest/controllers/InvoiceRestController.java
+++ src/main/java/com/entel/springboot/backend/apirest/controllers/InvoiceRestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +40,14 @@ public class InvoiceRestController {
 	
 	@GetMapping("invoices/product-filter/{term}")
 	@ResponseStatus(code=HttpStatus.OK)
-	public List<Product> productFilter(@PathVariable String term) {
+	public List<Product> productsFilter(@PathVariable String term) {
 		return clientService.findProductByName(term);
+	}
+	
+	@PostMapping("/invoices")
+	@ResponseStatus(code=HttpStatus.CREATED)
+	public Invoice create(@RequestBody Invoice invoice) {
+		return clientService.saveInvoice(invoice);
+		
 	}
 }
