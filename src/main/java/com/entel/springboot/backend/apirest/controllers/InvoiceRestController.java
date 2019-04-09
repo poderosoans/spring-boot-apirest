04e2cc71c392cc603b2d2d01bda41d812314eee7
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-09 12:54:45 -0500

    [INFO] - Sistema Facturación: Eliminando la factura y sus líneas.

--- src/main/java/com/entel/springboot/backend/apirest/controllers/InvoiceRestController.java
+++ src/main/java/com/entel/springboot/backend/apirest/controllers/InvoiceRestController.java
@@ -3,6 +3,7 @@ package com.entel.springboot.backend.apirest.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +25,11 @@ public class InvoiceRestController {
 	public Invoice show(@PathVariable Long id) {
 		
 		return clientService.findInvoiceById(id);
+	}
+	
+	@DeleteMapping("/invoices/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clientService.deleteInvoice(id);
 	}
 }
